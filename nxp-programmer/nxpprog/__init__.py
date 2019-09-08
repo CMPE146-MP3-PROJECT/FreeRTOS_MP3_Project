91d64e6936bd7c30ddb3f7135ff20c7dbd32da7f
Jason Tran <jasontran.one@gmail.com>
2019-09-08 18:27:55 +0000

    G#6: Add NXP programmer tool

---
+++ nxp-programmer/nxpprog/__init__.py
@@ -0,0 +1,14 @@
+"""
+`nxpprog` is a deviation of:
+- Repo: https://github.com/kammce/nxpprog.git
+- Commit: c77898412144f990c2feb5db7872f134b257ccbf
+- Changes: modified to support both Python 2.7.x and 3.x.x
+
+`serial` package is a copy of:
+- Repo: https://github.com/pyserial/pyserial.git
+- Tag: v3.4
+"""
+
+import os
+
+NXPPROG_PY = os.path.join(os.path.dirname(__file__), "nxpprog.py")
