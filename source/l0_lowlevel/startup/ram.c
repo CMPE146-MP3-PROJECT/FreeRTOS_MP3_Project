f5950c0c65b59d8472e573be40ba3ce22ba29771
Jason Tran <jasontran.one@gmail.com>
2019-09-08 13:59:16 -0700

    G#15: Fix RAM initialization

--- source/l0_lowlevel/startup/ram.c
+++ source/l0_lowlevel/startup/ram.c
@@ -21,13 +21,13 @@ void ram__init_bss(void)
 
 static void ram__init_data_sram_main(void)
 {
-    extern uint8_t *_bdata_lma;
-    extern uint8_t *_bdata_vma;
-    extern uint8_t *_edata;
+    extern uint8_t _bdata_lma;
+    extern uint8_t _bdata_vma;
+    extern uint8_t _edata;
 
-    const uint8_t *src = _bdata_lma;
-    uint8_t *dest = _bdata_vma;
-    while (src < _edata) {
+    uint8_t *src = &_bdata_lma;
+    uint8_t *dest = &_bdata_vma;
+    while (dest < &_edata) {
         *dest = *src;
         dest++;
         src++;
@@ -36,13 +36,13 @@ static void ram__init_data_sram_main(void)
 
 static void ram__init_data_sram_peripheral(void)
 {
-    extern uint8_t *_bdata_lma_peripheral;
-    extern uint8_t *_bdata_vma_peripheral;
-    extern uint8_t *_edata_peripheral;
+    extern uint8_t _bdata_lma_peripheral;
+    extern uint8_t _bdata_vma_peripheral;
+    extern uint8_t _edata_peripheral;
 
-    const uint8_t *src = _bdata_lma_peripheral;
-    uint8_t *dest = _bdata_vma_peripheral;
-    while (src < _edata_peripheral) {
+    uint8_t *src = &_bdata_lma_peripheral;
+    uint8_t *dest = &_bdata_vma_peripheral;
+    while (dest < &_edata_peripheral) {
         *dest = *src;
         dest++;
         src++;
@@ -51,11 +51,11 @@ static void ram__init_data_sram_peripheral(void)
 
 static void ram__init_bss_sram_main(void)
 {
-    extern uint8_t *_bbss;
-    extern uint8_t *_ebss;
+    extern uint8_t _bbss;
+    extern uint8_t _ebss;
 
-    uint8_t *ptr = _bbss;
-    while (ptr < _ebss) {
+    uint8_t *ptr = &_bbss;
+    while (ptr < &_ebss) {
         *ptr = 0U;
         ptr++;
     }
@@ -63,11 +63,11 @@ static void ram__init_bss_sram_main(void)
 
 static void ram__init_bss_sram_peripheral(void)
 {
-    extern uint8_t *_bbss_peripheral;
-    extern uint8_t *_ebss_peripheral;
+    extern uint8_t _bbss_peripheral;
+    extern uint8_t _ebss_peripheral;
 
-    uint8_t *ptr = _bbss_peripheral;
-    while (ptr < _ebss_peripheral) {
+    uint8_t *ptr = &_bbss_peripheral;
+    while (ptr < &_ebss_peripheral) {
         *ptr = 0U;
         ptr++;
     }
