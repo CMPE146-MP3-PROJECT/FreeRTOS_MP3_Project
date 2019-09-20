88b93deb51b0e4ae477714148ae9e04c84383787
Sree Harsha <sreeharsha@Srees-MacBook-Air.local>
2019-09-20 15:34:04 -0700

    Modified code as per review comments

--- lpc40xx_freertos/l3_drivers/pwm1.h
+++ lpc40xx_freertos/l3_drivers/pwm1.h
@@ -5,16 +5,14 @@
 
 #include <stdint.h>
 
-// Only the below PWM1 pins are available on SJ2 board
-// P2.0, P2.1, P2.2, P2.4, P2.5
+/// Only the below PWM1 channels are available on SJ2 board
 typedef enum {
-  PWM1__2_0 = 0,
-  PWM1__2_1 = 1,
-  PWM1__2_2 = 2,
-  PWM1__2_4 = 4,
-  PWM1__2_5 = 5,
-} pwm1_e;
+  PWM1__2_0 = 0, ///< Controls P2.0
+  PWM1__2_1 = 1, ///< Controls P2.1
+  PWM1__2_2 = 2, ///< Controls P2.2
+  PWM1__2_4 = 4, ///< Controls P2.4
+  PWM1__2_5 = 5, ///< Controls P2.5
+} pwm1_channel_e;
 
 void pwm1__init_single_edge(uint32_t frequency_in_hertz);
-void pwm1__set_frequency(uint32_t frequency_in_hertz);
-void pwm1__set_duty_cycle(pwm1_e pwm1_pin, double duty_cycle_in_percent);
+void pwm1__set_duty_cycle(pwm1_channel_e pwm1_channel, double duty_cycle_in_percent);
--- lpc40xx_freertos/l3_drivers/sources/pwm1.c
+++ lpc40xx_freertos/l3_drivers/sources/pwm1.c
@@ -4,26 +4,13 @@
 #include "lpc40xx.h"
 #include "lpc_peripherals.h"
 
-/*******************************************************************************
- *
- *                      P U B L I C    F U N C T I O N S
- *
- ******************************************************************************/
-
+/// For simple single edge PWM, all channels share the same frequency
 void pwm1__init_single_edge(uint32_t frequency_in_hertz) {
-  const uint32_t pwm_bits_9_to_14 = 0x3F;
-
-  lpc_peripheral__turn_on_power_to(LPC_PERIPHERAL__PWM1);
-  pwm1__set_frequency(frequency_in_hertz);
-
-  LPC_PWM1->MCR |= (1 << 1);                // Enable PWM reset when it matches MR0
-  LPC_PWM1->TCR = (1 << 0) | (1 << 3);      // Enable PWM counter
-  LPC_PWM1->PCR |= (pwm_bits_9_to_14 << 9); // Enable the PWM (bits 9-14)
-}
-
-void pwm1__set_frequency(uint32_t frequency_in_hertz) {
+  const uint32_t pwm_output_enable_mask = 0x3F;
   const uint32_t default_frequency_in_hertz = 50;
   uint32_t valid_frequency_in_hertz = frequency_in_hertz;
+
+  lpc_peripheral__turn_on_power_to(LPC_PERIPHERAL__PWM1);
 
   if (0 == frequency_in_hertz) {
     valid_frequency_in_hertz = default_frequency_in_hertz;
@@ -35,13 +22,17 @@ void pwm1__set_frequency(uint32_t frequency_in_hertz) {
   // Ex: If CPU freq = 10Hz, desired frequency = 2Hz
   // MR0 = 10/2 = 5. This means, TC will count upto 5 per pulse, generating 2 pulse per second
   LPC_PWM1->MR0 = match_reg_value;
+
+  LPC_PWM1->MCR |= (1 << 1);                      ///< Enable PWM reset when it matches MR0
+  LPC_PWM1->TCR = (1 << 0) | (1 << 3);            ///< Enable PWM counter
+  LPC_PWM1->PCR |= (pwm_output_enable_mask << 9); ///< Enable the PWM (bits 9-14)
 }
 
-void pwm1__set_duty_cycle(pwm1_e pwm1_pin, double duty_cycle_in_percent) {
+void pwm1__set_duty_cycle(pwm1_channel_e pwm1_channel, double duty_cycle_in_percent) {
   const uint32_t mr0_reg_val = LPC_PWM1->MR0;
   const uint32_t match_reg_value = (mr0_reg_val * duty_cycle_in_percent) / 100;
 
-  switch (pwm1_pin) {
+  switch (pwm1_channel) {
   case PWM1__2_0:
     LPC_PWM1->MR1 = match_reg_value;
     break;
@@ -61,5 +52,5 @@ void pwm1__set_duty_cycle(pwm1_e pwm1_pin, double duty_cycle_in_percent) {
     break;
   }
 
-  LPC_PWM1->LER |= (1 << (pwm1_pin + 1)); // Enable Latch Register
+  LPC_PWM1->LER |= (1 << (pwm1_channel + 1)); ///< Enable Latch Register
 }
