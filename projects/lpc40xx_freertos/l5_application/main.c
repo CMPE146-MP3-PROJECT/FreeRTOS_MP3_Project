6665c98a14762b233343a90a76c5a088fa8a6e34
Richielee630 <richielee630@yahoo.com>
2021-02-13 23:38:30 -0800

    lab 2 part 2.4

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -93,9 +93,9 @@ int main(void) {
 
   xTaskCreate(lab2_led_task, "LED0", 1024 / sizeof(void *), &led0, 1,
               NULL); /* &led0 is a task parameter going to led_task */
-  vTaskDelay(1500);
+  //vTaskDelay(1500);
   xTaskCreate(lab2_led_task, "LED1", 1024 / sizeof(void *), &led1, 1, NULL);
-  vTaskDelay(1500);
+  //vTaskDelay(1500);
 
   vTaskStartScheduler();
   return 0;
