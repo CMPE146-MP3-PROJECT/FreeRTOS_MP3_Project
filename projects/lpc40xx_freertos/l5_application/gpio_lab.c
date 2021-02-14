63a55a7d65f8ae67e290db5057ef08ab9f405ae7
Richielee630 <richielee630@yahoo.com>
2021-02-13 19:42:34 -0800

    lab 2 part 1.2

--- projects/lpc40xx_freertos/l5_application/gpio_lab.c
+++ projects/lpc40xx_freertos/l5_application/gpio_lab.c
@@ -1,9 +1,11 @@
 #include "gpio_lab.h"
 #include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
 //#include <cstdint.h>
 
 /// Should alter the hardware registers to set the pin as input
-void gpiox__set_as_input(port_pin_s pin_num) {
+void gpio0__set_as_input(port_pin_s pin_num) {
   switch (pin_num.port) {
   case 0:
     LPC_GPIO0->DIR &= ~(1 << pin_num.pin);
@@ -18,18 +20,19 @@ void gpiox__set_as_input(port_pin_s pin_num) {
 }
 
 /// Should alter the hardware registers to set the pin as output
-void gpiox__set_as_output(uint8_t pin_num) { LPC_GPIO1->DIR |= (1 << pin_num); }
+void gpio0__set_as_output(uint8_t pin_num) { LPC_GPIO1->DIR |= (1 << pin_num); }
 
 /// Should alter the hardware registers to set the pin as high
-void gpiox__set_high(uint8_t pin_num) { LPC_GPIO2->SET = (1 << pin_num); }
+void gpio0__set_high(uint8_t pin_num) { LPC_GPIO2->SET = (1 << pin_num); }
 
 /// Should alter the hardware registers to set the pin as low
-void gpiox__set_low(uint8_t pin_num) { LPC_GPIO2->CLR = (1 << pin_num); }
+void gpio0__set_low(uint8_t pin_num) { LPC_GPIO2->CLR = (1 << pin_num); }
 
-/*(void gpiox__set(port_pin_s pin_num, bool high) {
+void gpio0__set(port_pin_s pin_num, bool high) {
   if (high == 1) {
     gpiox__set_high(int pin_num);
   } else {
     gpiox__set_low(int pin_num);
   }
-}*/
+}
+bool gpio0__get_level(uint8_t pin_num);
--- projects/lpc40xx_freertos/l5_application/gpio_lab.h
+++ projects/lpc40xx_freertos/l5_application/gpio_lab.h
@@ -3,20 +3,24 @@
 #include "lpc40xx.h"
 #include <stdbool.h>
 #include <stdint.h>
+
 typedef struct {
   uint8_t port;
   uint8_t pin;
 } port_pin_s;
 
 /// Should alter the hardware registers to set the pin as input
-void gpiox__set_as_input(port_pin_s pin_num);
+void gpio0__set_as_input(port_pin_s pin_num);
 
 /// Should alter the hardware registers to set the pin as output
-void gpiox__set_as_output(uint8_t pin_num);
+void gpio0__set_as_output(uint8_t pin_num);
 
 /// Should alter the hardware registers to set the pin as high
-void gpiox__set_high(uint8_t pin_num);
+void gpio0__set_high(uint8_t pin_num);
 
 /// Should alter the hardware registers to set the pin as low
-void gpiox__set_low(uint8_t pin_num);
-void gpiox__set(port_pin_s pin_num, bool high);
+void gpio0__set_low(uint8_t pin_num);
+
+void gpio0__set(uint8_t pin_num, bool high);
+
+bool gpio0__get_level(uint8_t pin_num);
