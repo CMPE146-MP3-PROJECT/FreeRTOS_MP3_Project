806faa3a8f9e77a81b2401129402b40b9fd153ff
Richielee630 <richielee630@yahoo.com>
2021-02-13 23:47:49 -0800

    lab 2 part 2.4

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -62,8 +62,8 @@ static const uint32_t pin26 = (1 << 26); // 0x02000000? LED1后面写了: P2_3; 
 void lab2_led_task(void *task_parameter) {
   // Type-cast the paramter that was passed from xTaskCreate()
   port_pin_s *led_num = (port_pin_s *)(task_parameter);
-  //LPC_IOCON->P2_3 &= ~(7 << 0);
-  LPC_IOCON->P1_16 &= ~(7 << 0);
+  LPC_IOCON->P2_3 &= ~(7 << 0);
+  LPC_IOCON->P1_28 &= ~(7 << 0);
   gpiox__set_as_output(*led_num);
   while (true) {
     // do: insert code here to blink an LED
@@ -89,9 +89,9 @@ int main(void) {
   // Pass each task its own parameter:
   // This is static such that these variables will be allocated in RAM and not go out of scope
   static port_pin_s led0 = {2, 3};
-  static port_pin_s led1 = {1, 16};
+  static port_pin_s led1 = {1, 28};
 
-  //xTaskCreate(lab2_led_task, "LED0", 1024 / sizeof(void *), &led0, 1, NULL); /* &led0 is a task parameter going to
+  xTaskCreate(lab2_led_task, "LED0", 1024 / sizeof(void *), &led0, 1, NULL); /* &led0 is a task parameter going to
   // led_task */
   // vTaskDelay(1500);
   xTaskCreate(lab2_led_task, "LED1", 1024 / sizeof(void *), &led1, 1, NULL);
