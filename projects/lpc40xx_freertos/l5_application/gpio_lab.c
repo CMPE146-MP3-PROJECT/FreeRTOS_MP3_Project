88fd7641ac99d50a468a9afe4740cfd3f175c0ed
Richielee630 <richielee630@yahoo.com>
2021-02-13 19:44:47 -0800

    lab 2 part 1.2

--- projects/lpc40xx_freertos/l5_application/gpio_lab.c
+++ projects/lpc40xx_freertos/l5_application/gpio_lab.c
@@ -28,11 +28,11 @@ void gpio0__set_high(uint8_t pin_num) { LPC_GPIO2->SET = (1 << pin_num); }
 /// Should alter the hardware registers to set the pin as low
 void gpio0__set_low(uint8_t pin_num) { LPC_GPIO2->CLR = (1 << pin_num); }
 
-void gpio0__set(port_pin_s pin_num, bool high) {
+void gpio0__set(uint8_t pin_num, bool high) {
   if (high == 1) {
-    gpio0__set_high(int pin_num);
+    gpio0__set_high(uint8_t pin_num);
   } else {
-    gpio0__set_low(int pin_num);
+    gpio0__set_low(uint8_t pin_num);
   }
 }
 bool gpio0__get_level(uint8_t pin_num);
