6708d813d84bb4a41ce0738b0f3f2d4401fbfd65
Richielee630 <richielee630@yahoo.com>
2021-02-18 16:10:21 -0800

    lab 3 part 1 debugging

--- projects/lpc40xx_freertos/l5_application/gpio_isr.c
+++ projects/lpc40xx_freertos/l5_application/gpio_isr.c
@@ -4,13 +4,13 @@
 // Note: You may want another separate array for falling vs. rising edge callbacks
 static function_pointer_t gpio0_callbacks[32];
 
-void gpio0__attach_interrupt(uint32_t pin, gpio_interrupt_e interrupt_type, function_pointer_t callback) {
+void gpiox__attach_interrupt(port_pin_s pin, gpio_interrupt_e interrupt_type, function_pointer_t callback) {
   // 1) Store the callback based on the pin at gpio0_callbacks
   // 2) Configure GPIO 0 pin for rising or falling edge
 }
 
 // We wrote some of the implementation for you
-void gpio0__interrupt_dispatcher(void) {
+void gpiox__interrupt_dispatcher(void) {
   // Check which pin generated the interrupt
   const int pin_that_generated_interrupt = logic_that_you_will_write();
   function_pointer_t attached_user_handler = gpio0_callbacks[pin_that_generated_interrupt];
